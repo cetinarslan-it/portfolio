1609d85836ba515949c6a37d27507e77ce4a459d
cetinarslan <cetinarslan.se@gmail.com>
2022-09-06 11:27:05 +0200

    Added particals for background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import Navigation from './screens/navigationBar/NavigationBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Navigation from "./screens/navigationBar/NavigationBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from './Particles';
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
+      <Navigation />
+      <Particles/>
+
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -1,30 +1,49 @@
-import React from 'react'
-import { Navbar } from 'react-bootstrap'
-import Nav from 'react-bootstrap/Nav'
-import { navigationLinks } from '../../helpers/navigationLinks'
-import './navigation.css'
+import React from "react";
+import "./navigation.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { navigationLinks } from "../../helpers/navigationLinks";
 
-function createLinks(){
+function createLinks() {
   return navigationLinks.map((e, idx) => (
-    <Nav.Link style={{margin: '1rem' }} key={idx} href={e.ref}>{e.name}</Nav.Link>
-  ))
+    <Nav.Link key={idx} href={e.ref}>
+      {e.name}
+    </Nav.Link>
+  ));
 }
 
 function NavigationBar() {
-
   return (
     <div id="home">
-      <Navbar className="navigation__container" expand="md">
-        <Navbar.Brand style={{marginLeft: '2rem'}} href="#home">Cetin Arslan</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse style= {{justifyContent: 'flex-end', marginRight: '1rem' }}>
-          <Nav className="links" style={{margin: '0rem' }}>
+      <Navbar
+        className="navigation__container"
+        style={{
+          zIndex: "2",
+          position: "fixed",
+          top: "0",
+          width: "100%",
+        }}
+        collapseOnSelect
+        expand="md"
+      >
+        <Navbar.Brand style={{ marginLeft: "1rem" }} href="#home">
+          Cetin ARSLAN
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          style={{
+            justifyContent: "flex-end",
+            marginRight: "1rem",
+            borderColor: "none",
+          }}
+        >
+          <Nav className="links" style={{ margin: "0 1rem" }}>
             {createLinks()}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     </div>
-  )
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
