00ba68f5bd2464ab4f48054ba722a1d169b90f3c
cetinarslan <cetinarslan.se@gmail.com>
2022-09-08 12:59:34 +0200

    Refactored skills

--- src/screens/skills/Skills.jsx
+++ src/screens/skills/Skills.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./skills.css";
+import DevIcon from "devicon-react-svg";
+import { techList } from "../../helpers/techList";
 
 function Skills() {
   return (
-    <div className="skills-container" id="skills">
-      <h1>Skills</h1>
+    <div className="technologies__container" id="skills">
+      <div>
+        <h1 style={{ textAlign: "center", fontWeight: "bold" }}>
+          Some of the skills that I use...
+        </h1>
+      </div>
+      <div className="tech__cards__container">
+        {techList.map((e, index) => {
+          return (
+            <a href={e.link} target="_blank" rel="noreferrer">
+              <div key={index} className="tech__card">
+                <DevIcon className="tech__icons" icon={e.iconName} />
+                <span
+                  style={{
+                    margin: "5px",
+                    fontSize: "2rem",
+                    fontWeight: "Bold",
+                  }}
+                >
+                  {e.name}
+                </span>
+              </div>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 }
