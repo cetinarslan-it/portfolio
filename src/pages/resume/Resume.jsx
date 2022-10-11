521dd33ce29a84a4208a4b5fbbf3061074658c40
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-10-11 18:55:25 +0200

    Added some icons and refactored resume component

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./resume.css";
 import { resumeList } from "../../repositories/resumeList";
 
-
-
 function Resume() {
   return (
     <div>
@@ -14,25 +12,27 @@ function Resume() {
         <div className="container">
           {resumeList.map((e, index) => {
             return (
-              <div className="main-timeline">               
+              <div className="main-timeline">
                 <div className={e.dir}>
                   <div className="card">
                     <div className="card-body p-3 d-lg-flex">
                       <div>
-                      <img
-                        src={e.src}
-                        className="card-img-top me-4 mb-4"
-                        alt=".."
-                        style={{ height: "120px", width: "120px" }}
-                      /> {console.log(e.src)}
+                        <img
+                          src={e.src}
+                          className="card-img-top me-4 mb-4"
+                          alt=".."                         
+                          style={{height: "100px", width: "120px"}}
+                        />
                       </div>
                       <div>
                         <h3>{e.date}</h3>
+                        <p className="mb-0 exp-title">{e.title}</p>
+                        <hr></hr>
                         <p className="mb-0">{e.exp}</p>
                       </div>
                     </div>
                   </div>
-                </div>      
+                </div>
               </div>
             );
           })}
--- src/repositories/resumeList.js
+++ src/repositories/resumeList.js
@@ -1,38 +1,51 @@
 export const resumeList = [
     {
-        src: require("../images/myPicture.png"),
-        date: "2022-Now",
-        exp: "Under construction",
+        src: require("../images/icons/salt-icon.png"),
+        date: "2022/Agu - Now",
+        title:"Full Stack .NET Developer at </salt>",
+        exp: "Consulting in .NET full Stack development. Applied skills include C#, JavaScript, React, Node.js, HTML, CSS, SQL, TDD & agile mob programming. Consulting in .NET full stack development.",
+        dir: "timeline left"
+    },
+    {
+        src: require("../images/icons/student-icon.png"),
+        date: "2022/May - 2022/June",
+        title:"Full Stack .NET - School of Applied Technology </salt>",
+        exp: "From over 1500 applicants, I was one of ~30 students selected to partake in an intensive Full Stack .NET Developer training program. It is an intensive three month program for full stack web development with a focus on TDD, mob programming, and applied learning.",
         dir: "timeline right"
     },
     {
-        src: require("../images/background.jpg"),
+        src: require("../images/icons/agileprocrm-icon.png"),
         date: "2021-2022",
-        exp: "Under construction",
+        title: "Front-End Automation Tester(SDET)",
+        exp: "Worked as an automation tester on front-end in agile/scrum using Java, IntelliJ, Maven, Selenium, Cucumber and TestNG",
         dir: "timeline left"
     },
     {
-        src: require("../images/myPicture.png"),
+        src: require("../images/icons/bookofid-icon.png"),
         date: "2020-2021",
-        exp: "Under construction",
+        title:"Back-End Manual Tester(SDET)",
+        exp: "Worked as a manual tester on back-end using the tool Postman and experienced documentation in bug tracking.",
         dir: "timeline right"
     },
     {
-        src: require("../images/background.jpg"),
+        src: require("../images/icons/student-icon.png"),
         date: "2018-2020",
-        exp: "Under construction",
+        title:"",
+        exp: "Relocation process, Language courses (English and Swedish), Attended bootcamps about Software Manual and Automation Testing.",
         dir: "timeline left"
     },
     {
-        src: require("../images/myPicture.png"),
+        src: require("../images/icons/manager-icon.png"),
         date: "2005-2016",
-        exp: "Under construction",
+        title:"Administrator – Ministry of Interior, Turkey",
+        exp: "Worked as an administrator in the public sector for more than 10 years and managed/inspected public institutions as a part of quality management",
         dir: "timeline right"
     },
     {
-        src: require("../images/background.jpg"),
+        src: require("../images/icons/student-icon.png"),
         date: "2001-2005",
-        exp: "Under construction",
+        title: "Bachelor’s Degree - System Engineer – National Defense University/Ankara",
+        exp: "Graduated in 2005 as a \"System Engineer\", focused on mechanical engineering courses. Some of the courses: Computer Science, Information Systems, Machinary Systems, System  Simulations, Technical Drawing, Statistics, etc.  ",
         dir: "timeline left"
     },
 ];
