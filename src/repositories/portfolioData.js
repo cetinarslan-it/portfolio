5c844a26ffd497a4b624a61b2f708e2124df50bb
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-10-12 11:23:15 +0200

    Added NZWalk.gif to portfolio

--- src/repositories/portfolioData.js
+++ src/repositories/portfolioData.js
@@ -1,6 +1,6 @@
 import StudentPortal from "../images/projectGIFs/StudentPortal.gif";
-/*import NZWalksAPI from "../images/projectGIFs/NZWalks.gif";
-import ToDoApp from "../images/projectGIFs/Todo-react.gif";*/
+import NZWalksAPI from "../images/projectGIFs/NZWalks.gif";
+/*import ToDoApp from "../images/projectGIFs/Todo-react.gif";*/
 
 const portfolioData = [
  {
@@ -9,14 +9,14 @@ const portfolioData = [
     desc: "Student Portal",
     summary: "An app for students to reach educational materials",
     tech: "React, Javascript|HTML5|CSS3, MUI",
-  },/*
+  },
   {
     image: NZWalksAPI,
     link: "https://github.com/cetinarslan-it/NZWalks",
     desc: "ASP.Net Web API with JWT",
     summary: "A web API project with authorization and authentication",
     tech: "ASP.Net Web API, C#, Sql Server MS",
-  },
+  },/*
   {
     image: ToDoApp,
     link: "https://github.com/cetinarslan-it/todoreact",
