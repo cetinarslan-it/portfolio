f4a4ef75459ecae0c480dbea726c2779533ba36a
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-10-12 11:14:27 +0200

    Added StudentPortal.gif to Portfolio

--- src/repositories/portfolioData.js
+++ src/repositories/portfolioData.js
@@ -1,15 +1,15 @@
-/*import StudentPortal from "../images/projectGIFs/StudentPortal.gif";
-import NZWalksAPI from "../images/projectGIFs/NZWalks.gif";
+import StudentPortal from "../images/projectGIFs/StudentPortal.gif";
+/*import NZWalksAPI from "../images/projectGIFs/NZWalks.gif";
 import ToDoApp from "../images/projectGIFs/Todo-react.gif";*/
 
 const portfolioData = [
- /* {
+ {
     image: StudentPortal,
     link: "https://github.com/cetinarslan-it/StudentPortal",
     desc: "Student Portal",
     summary: "An app for students to reach educational materials",
     tech: "React, Javascript|HTML5|CSS3, MUI",
-  },
+  },/*
   {
     image: NZWalksAPI,
     link: "https://github.com/cetinarslan-it/NZWalks",
