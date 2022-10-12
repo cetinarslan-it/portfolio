8567ba489ce696482cecf1ed1f6de6e2c50188b8
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-10-12 10:52:28 +0200

    Removed two gifs

--- src/repositories/portfolioData.js
+++ src/repositories/portfolioData.js
@@ -10,20 +10,6 @@ const portfolioData = [
     summary: "An app for students to reach educational materials",
     tech: "React, Javascript|HTML5|CSS3, MUI",
   },
-  {
-    image: NZWalksAPI,
-    link: "https://github.com/cetinarslan-it/NZWalks",
-    desc: "ASP.Net Web API with JWT",
-    summary: "A web API project with authorization and authentication",
-    tech: "ASP.Net Web API, C#, Sql Server MS",
-  },
-  {
-    image: ToDoApp,
-    link: "https://github.com/cetinarslan-it/todoreact",
-    desc: "To-Do App",
-    summary: "To-do app created in React.js library",
-    tech: "React.js, JavaScript, CSS3",
-  },
 ];
 
 export default portfolioData;
