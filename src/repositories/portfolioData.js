2f26a90433479dadb4b477fdfca97c80cf0618a7
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-10-12 10:49:01 +0200

    Added project gifs to portfolio component

--- src/repositories/portfolioData.js
+++ src/repositories/portfolioData.js
@@ -1,9 +1,29 @@
-//import StudentPortal from "../images/projectGIFs/StudentPortal.gif";
-//import NZWalksAPI from "../images/projectGIFs/NZWalks.gif";
-//import ToDoApp from "../images/projectGIFs/Todo-react.gif";
+import StudentPortal from "../images/projectGIFs/StudentPortal.gif";
+import NZWalksAPI from "../images/projectGIFs/NZWalks.gif";
+import ToDoApp from "../images/projectGIFs/Todo-react.gif";
 
 const portfolioData = [
-  
+  {
+    image: StudentPortal,
+    link: "https://github.com/cetinarslan-it/StudentPortal",
+    desc: "Student Portal",
+    summary: "An app for students to reach educational materials",
+    tech: "React, Javascript|HTML5|CSS3, MUI",
+  },
+  {
+    image: NZWalksAPI,
+    link: "https://github.com/cetinarslan-it/NZWalks",
+    desc: "ASP.Net Web API with JWT",
+    summary: "A web API project with authorization and authentication",
+    tech: "ASP.Net Web API, C#, Sql Server MS",
+  },
+  {
+    image: ToDoApp,
+    link: "https://github.com/cetinarslan-it/todoreact",
+    desc: "To-Do App",
+    summary: "To-do app created in React.js library",
+    tech: "React.js, JavaScript, CSS3",
+  },
 ];
 
 export default portfolioData;
