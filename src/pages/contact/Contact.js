52c579d768b6f88841831a8334c079d3753de00f
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-10-16 12:41:48 +0200

    added phone number to form

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -14,29 +14,36 @@ function Contact() {
         >
           <input type="hidden" name="form-name" value="contact" />
 
-          <label htmlFor="firstname">First Name <br /></label>
+          <label htmlFor="firstname">First Name* <br /></label>
           <input id="fistname" name="firstname" placeholder="Cetin" required />
 
-          <label htmlFor="lastname">Last Name <br /></label>
+          <label htmlFor="lastname">Last Name*<br /></label>
           <input id="lastname" name="lastname" placeholder="Arslan" required />
 
-          <label htmlFor="email">Email <br /></label>
+          <label htmlFor="phonenumber">Phone Number<br /></label>
+          <input id="phonenumber" name="phonenumber" placeholder="XXX-XXX-XX-XX" />
+
+          <label htmlFor="email">Email* <br /></label>
           <input
             id="email"
             name="email"
             type="email"
             placeholder="cetinarslan@email.com"
             required
-          />
-          
-          <label htmlFor="textarea">Your message</label>
+          />  
+
+          <label htmlFor="textarea">Your message*</label>
           <textarea
             id="textarea"
             name="message"
             placeholder="Type your message here..."
             required
           />
+
           <input type="submit" />
+
+          <p style={{textAlign:"left", fontSize:"0.8rem"}}>All the info with * sign are required. Please fill in the required ones...</p>
+          
         </form>
       </div>
     </div>
